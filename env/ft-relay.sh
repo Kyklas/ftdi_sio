33712fd40205f5515b331c40e5859afc0e79754b
Stan Bertrand <sbertrand@witekio.com>
2023-10-02 17:30:48 -0700

    env: allow multiple configuration

--- env/ft-relay.sh
+++ env/ft-relay.sh
@@ -2,6 +2,66 @@
 
 # Source this script
 
+SOURCED=0
+if [ -n "$BASH_VERSION" ]; then
+  (return 0 2>/dev/null) && SOURCED=1
+else
+	echo "Use script from bash"
+fi
+
+if [ ${SOURCED} -eq 0 ]; then
+	echo "Script should be sourced"
+	exit 1
+fi
+
+RELAY_NAME=
+PIN_PWR=
+PIN_INP=
+PIN_BOOT=
+PIN_IGN=
+
+function ft_config() {
+	
+	echo "Setup Relay Config"
+	local OPTIND flag
+	
+	while getopts hc:p:i:b:g: flag
+	do
+		case "${flag}" in
+			c)
+				case "${OPTARG}" in
+					1)
+						echo "Using Config 1"
+						RELAY_NAME=ftdi-cbus-FTRelay1
+						PIN_PWR=1
+						PIN_INP=0
+						PIN_BOOT=3
+						PIN_IGN=2
+						PIN_INP_INV=1
+						;;
+					2)
+						echo "Using Config 2"
+						RELAY_NAME=ftdi-cbus-FTRelay2
+						PIN_PWR=2
+						PIN_INP=1
+						PIN_BOOT=0
+						PIN_IGN=3
+						PIN_INP_INV=0
+						;;
+				esac
+			;;
+		esac
+	done
+
+	echo "Relay ${RELAY_NAME}"
+	echo "- Power   : ${PIN_PWR}"
+	echo "- Input   : ${PIN_INP}"
+	echo "- Boot    : ${PIN_BOOT}"
+	echo "- Ignition: ${PIN_IGN}"
+}
+
+ft_config $*
+
 function ftlog(){
     echo "FT Relay: $@" >&2
 }
@@ -26,13 +86,15 @@ function ftpwrset() {
     state=$(ft_arg $1)
     [ -z "${state}" ] && return
     ftlog "Power set ${state}"
-    gpioset ftdi-cbus-FTRelay1 1=${state}
+    gpioset ${RELAY_NAME} ${PIN_PWR}=${state}
     echo ${state}
 }
 
 function ftinput()
 {
-    input=$(gpioget -l ftdi-cbus-FTRelay1 0)
+	local PARAM=
+	[ ${PIN_INP_INV} -eq 1 ] && PARAM=-l
+    input=$(gpioget ${PARAM} ${RELAY_NAME} ${PIN_INP})
     ftlog "Input reads: $input"
     echo $input
 }
@@ -52,7 +114,7 @@ function ftig() {
     state=$(ft_arg $1)
     [ -z "${state}" ] && return
     ftlog "Ignition set ${state}"
-    gpioset ftdi-cbus-FTRelay1 2=${state}
+    gpioset ${RELAY_NAME} ${PIN_IGN}=${state}
     echo ${state}
 }
 
@@ -60,7 +122,7 @@ function ftboot() {
     state=$(ft_arg $1)
     [ -z "${state}" ] && return
     ftlog "Boot set ${state}"
-    gpioset ftdi-cbus-FTRelay1 3=${state}
+    gpioset ${RELAY_NAME} ${PIN_BOOT}=${state}
     echo ${state}
 }
 
